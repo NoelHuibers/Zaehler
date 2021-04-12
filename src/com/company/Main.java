6bf4938f07a81164787af3987f9d87cf1bef6d02
NoelHuibers <Noel_Huibers@web.de>
2021-04-12 23:30:37 +0200

    Test

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    int y = 0;
+    public static void main(String[] args) {
+	// code
+    }
+    public int addieren (int x){
+        y=+x;
+        return x;
+    }
+}
